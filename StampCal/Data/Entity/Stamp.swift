344db4ab61d2f30af628d8f8e145468e999c9501
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2023-09-10 01:06:24 +0900

    スタンプ一覧画面の実装WIP

--- StampCal/Data/Entity/Stamp.swift
+++ StampCal/Data/Entity/Stamp.swift
@@ -20,6 +20,15 @@ struct Stamp: Identifiable {
         Stamp(emoji: "🏃", summary: "運動"),
         Stamp(emoji: "🛠️", summary: "開発"),
         Stamp(emoji: "🛁", summary: "風呂洗い"),
-        Stamp(emoji: "📝", summary: "英語の勉強")
+        Stamp(emoji: "📝", summary: "英語の勉強"),
+        Stamp(emoji: "🗣️", summary: "人と話す"),
+        Stamp(emoji: "🍞", summary: "朝食"),
+        Stamp(emoji: "🍱", summary: "昼食"),
+        Stamp(emoji: "🍛", summary: "夕食"),
+        Stamp(emoji: "🧘", summary: "瞑想"),
+        Stamp(emoji: "🏆", summary: "優勝"),
+        Stamp(emoji: "🧩", summary: "パズル"),
+        Stamp(emoji: "🏊‍♀️", summary: "水泳"),
+        Stamp(emoji: "🎸", summary: "ギターの練習")
     ]
 }
--- StampCal/Presentation/View/Stamps/StampsView.swift
+++ StampCal/Presentation/View/Stamps/StampsView.swift
@@ -10,14 +10,61 @@ import SwiftUI
 
 struct StampsView: View {
     @StateObject var viewModel = StampsViewModel()
-    private let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    private let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16)
+    ]
 
     var body: some View {
-        ScrollView(.vertical) {
-            LazyVGrid(columns: columns) {
-                ForEach(viewModel.stamps) { stamp in
-                    Text("")
+        VStack(spacing: 0) {
+            // Header
+            VStack(spacing: 0) {
+                ZStack {
+                    HStack {
+                        Spacer()
+                        Button {
+                            viewModel.addStamp()
+                        } label: {
+                            Image(systemName: "plus")
+                                .font(.title2)
+                        }
+                    }
+                    Text("stamps")
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
                 }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 16)
+                Divider()
+            }
+            .background(SCColor.toolbarBackground)
+            // Body
+            ScrollView(.vertical) {
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(viewModel.stamps) { stamp in
+                        RoundedRectangle(cornerRadius: 8)
+                            .aspectRatio(1, contentMode: .fill)
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(SCColor.cellBackground)
+                            .shadow(color: SCColor.shadow, radius: 3, x: 0, y: 3)
+                            .overlay {
+                                VStack(spacing: 0) {
+                                    Text(String(stamp.emoji))
+                                        .font(.largeTitle)
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                                        .background(SCColor.cellHighlightWeek)
+                                    Text(stamp.summary)
+                                        .lineLimit(1)
+                                        .minimumScaleFactor(0.5)
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                                        .padding(4)
+                                }
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                            }
+                    }
+                }
+                .padding(24)
             }
         }
         .background(SCColor.appBackground)
