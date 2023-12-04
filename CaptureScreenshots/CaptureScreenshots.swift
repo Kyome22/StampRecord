387933755bf3a6d136ef72d635b65245a38a6825
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2023-12-04 22:57:38 +0900

    Add CaptureScreenshots (UI Tests利用)

---
+++ CaptureScreenshots/CaptureScreenshots.swift
@@ -0,0 +1,16 @@
+/*
+ CaptureScreenshots.swift
+ CaptureScreenshots
+
+ Created by Takuto Nakamura on 2023/10/23.
+ Copyright © 2023 Studio Kyome. All rights reserved.
+*/
+
+import XCTest
+
+final class CaptureScreenshots: XCTestCase {
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+}
